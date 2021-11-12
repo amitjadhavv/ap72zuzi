2b1b33f53681d47e379fd52721fad49f7ec1167f
amitjadhavv <amitaaj123@gmail.com>
2021-11-12 14:33:39 +0100

    comments: committing my first package

--- ap72zuzi/function.py
+++ ap72zuzi/function.py
@@ -3,7 +3,6 @@ from ipywidgets import interact, fixed
 from PIL import Image
 
 def imshow(X, resize=None):
-    print(X,resize)
     image=Image.fromarray(X)
     if resize!=None:
         image = image.resize(resize)
