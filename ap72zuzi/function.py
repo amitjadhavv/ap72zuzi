eae67b9288d1ae0a36cdf1a4d3531a570da9b80e
amitjadhavv <amitaaj123@gmail.com>
2021-11-14 15:32:40 +0100

    new update

--- ap72zuzi/function.py
+++ ap72zuzi/function.py
@@ -1,9 +1,11 @@
 import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
+from PIL import Image
+import matplotlib.pyplot as plt
 
 def imshow(X, resize=None):
     image=Image.fromarray(X)
     if resize!=None:
         image = image.resize(resize)
-    return image.show()
+    return plt.imshow(image)
